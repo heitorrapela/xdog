e2b9f5922f42b842f58bd2a73c42272f2c7a3d0b
heitorrapela <hrm@cin.ufpe.br>
2018-01-31 15:56:28 -0300

    Add difference of gaussians

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import cv2
 import numpy as numpy
 
-img = cv2.imread('imgs/lena.png')
+def dog(img,size1=(7,7),size2=(5,5),sigma1=0,sigma2=0):
+	if(size1[0] < size2[0]):
+		aux = size1
+		size1 = size2
+		size2 = aux
+	img1 = cv2.GaussianBlur(img,size1,sigma1)
+	img2 = cv2.GaussianBlur(img,size2,sigma2)
+	return img1-img2
 
-cv2.imshow("Lena",img)
-cv2.waitKey(0)
+if __name__ == '__main__':
+	# Open image in grayscale
+	img = cv2.imread('imgs/lena.png',0)
+	cv2.imshow("Lena",dog(img))
+	cv2.waitKey(0)
