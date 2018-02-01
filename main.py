9ef4889c5028b09f1b5ee3bedb1ce1752647e464
heitorrapela <hrm@cin.ufpe.br>
2018-02-01 09:37:52 -0300

    Fix implementation, and put two xdog versions: mine and garygrossi

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from scipy.ndimage.filters import gaussian_filter
 import time
 from matplotlib import pyplot as plt
 
-def timed(f):
-  start = time.time()
-  ret = f()
-  elapsed = time.time() - start
-  return ret, elapsed
-
 def calculateHistogram3d(img):
 	color = ('b','g','r')
 	img = abs(img)
@@ -23,38 +17,14 @@ def calculateHistogram1d(img):
 	plt.hist(img.ravel(),256,[0,256]); plt.show()
 
 # Difference of Gaussians applied to img input
-# Debug
 def dog(img,size=(0,0),k=1.6,sigma=0.5,gamma=1):
-	start = time.time()
-	img1 = cv2.GaussianBlur(img,size,sigma)
-	elapsed = time.time() - start
-	print elapsed
 	img1 = cv2.GaussianBlur(img,size,sigma)
 	img2 = cv2.GaussianBlur(img,size,sigma*k)
-
-	start = time.time()	
-	gauss1 = gaussian_filter(img, sigma)
-	elapsed = time.time() - start
-	print elapsed
-
-	print "GAUSS1: ", gauss1[0][0]
-	gauss2 = gamma*gaussian_filter(img, sigma*k)
-	print gauss1
-	differenceGauss = gauss1 - gauss2
-	#return differenceGauss
-
-	print type(img1), img.shape, np.count_nonzero(img1)
-	print type(gauss1), gauss1.shape, np.count_nonzero(gauss1)
-	ret = img1-gauss1
-	print np.count_nonzero(ret)
-	print ret.shape
-	calculateHistogram1d(ret)
 	return (img1-gamma*img2)
 
 # Threshold the dog image, with dog(sigma,k) > 0 ? 1(255):0(0)
-'''
-def edge_dog(img,sigma=0.5,k=1.6):
-	aux = dog(img,sigma=sigma,k=k)
+def edge_dog(img,sigma=0.5,k=200,gamma=0.98):
+	aux = dog(img,sigma=sigma,k=k,gamma=0.98)
 	for i in range(0,aux.shape[0]):
 		for j in range(0,aux.shape[1]):
 			if(aux[i,j] > 0):
@@ -62,36 +32,80 @@ def edge_dog(img,sigma=0.5,k=1.6):
 			else:
 				aux[i,j] = 0
 	return aux
-'''
 
-def xdog(img,sigma=0.5,k=1.6, gamma=1,epsilon=1,phi=1):
-	print sigma, k , gamma, phi
+# xdog adapted from garygrossi
+def xdog_garygrossi(img,sigma=0.5,k=200, gamma=0.98,epsilon=0.1,phi=10):
 	aux = dog(img,sigma=sigma,k=k,gamma=gamma)/255
 	for i in range(0,aux.shape[0]):
 		for j in range(0,aux.shape[1]):
 			if(aux[i,j] >= epsilon):
 				aux[i,j] = 1
 			else:
-				#aux[i,j] = 255 + np.tanh(phi*(aux[i,j]))
 				ht = np.tanh(phi*(aux[i][j] - epsilon))
 				aux[i][j] = 1 + ht
 	return aux*255
 
+# version of xdog inspired by article
+def xdog(img,sigma=0.5,k=1.6, gamma=1,epsilon=1,phi=1):
+	aux = dog(img,sigma=sigma,k=k,gamma=gamma)/255
+	for i in range(0,aux.shape[0]):
+		for j in range(0,aux.shape[1]):
+			if(aux[i,j] < epsilon):
+				aux[i,j] = 1*255
+			else:
+				aux[i,j] = 255*(1 + np.tanh(phi*(aux[i,j])))
+	return aux
+
+
 if __name__ == '__main__':
 	# Open image in grayscale
-	img = cv2.imread('imgs/rapela.jpg',cv2.CV_LOAD_IMAGE_GRAYSCALE)
-		
+	#img = cv2.imread('imgs/rapela.jpg',cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	
+	img = cv2.imread('imgs/lena.png',cv2.CV_LOAD_IMAGE_GRAYSCALE)
+
 	# k = 1.6 as proposed in the paper
 	k = 1.6
 
 	#cv2.imshow("Lena", dog(img))
 	#edge_dog(img)
-	#cv2.imshow("tessst",edge_dog(img))
+	#cv2.imshow("tessst",edge_dog(img,sigma=0.5,k=200, gamma=0.98))
 	#cv2.waitKey(0)
 	#cv2.imshow("Lena",dog(img,gamma=1))
 	#cv2.waitKey(0)
-	cv2.imshow("XBUDOG",np.uint8(xdog(img,sigma=0.5,k=200, gamma=0.98,epsilon=0.1,phi=10)))
-	#result = xdog(img)
-	#cv2.imshow("Image",np.uint8(result))
+	cv2.imshow("XBUDOG",np.uint8(xdog_garygrossi(img,sigma=0.5,k=200, gamma=0.98,epsilon=0.1,phi=10)))
+	#cv2.imshow("XBUDOG2",np.uint8(xdog_garygrossi(img)))
+	cv2.imshow("XBUDOG_mine",np.uint8(xdog(img,sigma=0.4,k=1.6, gamma=0.5,epsilon=-0.5,phi=10)))
+	
+	# Test
+	cv2.imshow("XBUDOG_mineTest",np.uint8(xdog(img,sigma=1.6,k=1.6, gamma=0.5,epsilon=-1,phi=10)))
+
+	# Natural media (tried to follow parameters of article)
+	cv2.imshow("Natural Media",np.uint8(xdog(img,sigma=1,k=1.6, gamma=0.5,epsilon=-0.5,phi=10)))
+
 	cv2.waitKey(0)
-	#cv2.imwrite("res.jpg", result)
+
+'''
+# Video version - Its not real time version :)
+if __name__ == '__main__':
+	# Open image in grayscale
+	cap = cv2.VideoCapture(0)
+		
+	# k = 1.6 as proposed in the paper
+	k = 1.6
+
+	while(True):
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		# Display the resulting frame
+		cv2.imshow("Natural Media",np.uint8(xdog(gray,sigma=1,k=1.6, gamma=0.5,epsilon=-0.5,phi=10)))
+		cv2.imshow("Gary",np.uint8(xdog_garygrossi(gray)))
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()
+'''
